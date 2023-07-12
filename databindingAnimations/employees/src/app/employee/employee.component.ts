bcee59fbb979201df62fb164e040b8f64cbaa66b
abibou1 <abibou2017@gmail.com>
2023-07-12 16:41:01 -0400

    added switching edit/update

--- databindingAnimations/employees/src/app/employee/employee.component.ts
+++ databindingAnimations/employees/src/app/employee/employee.component.ts
@@ -8,5 +8,54 @@ import { Employee } from './employee';
   styleUrls: ['./employee.component.css']
 })
 export class EmployeeComponent {
+  employees: Employee[] = [];
+  showEdit: boolean = false;
 
+  constructor() {}
+
+  ngOnInit() {
+
+    this.employees = [
+
+      {
+        employee_id: 1,
+        first_name: "John",
+        last_name: "Doe",
+        salary: 21000,
+        dob: "01/01/1999",
+        email: "john@ggmail.com",
+        action: "submit"
+
+      },
+
+      {
+        employee_id: 1,
+        first_name: "John",
+        last_name: "Doe",
+        salary: 21000,
+        dob: "01/01/1999",
+        email: "john@ggmail.com",
+        action: "submit"
+      },
+      {
+        employee_id: 2,
+        first_name: "Johnny",
+        last_name: "Dang",
+        salary: 30000,
+        dob: "01/01/1989",
+        email: "johnny@ggmail.com",
+        action: "submit"
+
+      }
+    ]
+  }
+
+  editEmp(){
+    this.showEdit = true;
+  }
+  
+  updateEmp(){
+    this.showEdit = false;
+  }
+  
 }
