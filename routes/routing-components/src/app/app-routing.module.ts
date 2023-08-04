b85a256bd61a6c054848e2f612bf5512c1f15a0b
abibou1 <abibou2017@gmail.com>
2023-08-04 00:54:45 -0400

    generated components and defined route constant

--- routes/routing-components/src/app/app-routing.module.ts
+++ routes/routing-components/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'product',
+    component: ProductComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
